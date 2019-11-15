76912138f37cd272dfd14417095a1605a2b68072
Anh <nd.anh@hutech.edu.vn>
2019-11-15 07:32:04 +0700

    Add project files.

---
+++ FunnyHutech/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunnyHutech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
