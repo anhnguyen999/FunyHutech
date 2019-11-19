bc8b9bc3e7731c6eefaee1b59ff8952e73191ce8
Admin <Admin@DESKTOP-7MD3I4S>
2019-11-19 07:32:33 +0700

    ok

--- Student/Program.cs
+++ Student/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Nguyen Trong Duong
+/// Do Thanh so
+/// </summary>
 namespace Student
 {
     class Program
