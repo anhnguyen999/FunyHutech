e12d97b6bec7ac8b7eabcdd061c78bb60b0278e0
Admin <Admin@DESKTOP-7MD3I4S>
2019-11-19 08:30:47 +0700

    nopbaitap3.3 nhom Cthang

--- Student/Program.cs
+++ Student/Program.cs
@@ -5,30 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 /// <summary>
 /// Nguyen Trong Duong
-/// Do Thanh son
-/// 
+/// Do Thanh Son
+/// Vu Tri Toan
 /// </summary>
 namespace Student
 {
     class Program
     {
+        static int numOfStudent = 0;
+        static Student[] studentList;
         static void Main(string[] args)
         {
-            Console.Write("Number of Student = ");
             //lay so luong sinh vien do nguoi dung nhap
             //ctrl + k + c: comment
             //ctrl + k + u: uncomment
             //ctrl + k + d: lam dep code, format code
-            int numOfStudent;
-            try
+            do
             {
-                numOfStudent = int.Parse(Console.ReadLine());
-                InputStudentList(numOfStudent);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Invalid Input");
-            }
+                Console.Write("Input Number Of Student: ");
+            } while (!int.TryParse(Console.ReadLine(), out numOfStudent));
+            //do..while de k bi thoat 
+            Console.WriteLine("\n=====INPUT STUDENT LIST=====");
+            InputStudentList(numOfStudent);
+            Console.WriteLine("\n=====SHOW THE LIST OF STUDENT=====");
+            ShowStudentList(studentList);
+            ShowFacultyCNTT();
             //dung man hinh de xem ket qua
             Console.ReadKey();
         }
@@ -36,16 +37,46 @@ namespace Student
         private static void InputStudentList(int numOfStudent)
         {
             //tao mang danh sach sinh vien
-            Student[] studentList = new Student[numOfStudent];
+            studentList = new Student[numOfStudent];
             Student student;
             for (int i = 0; i < numOfStudent; i++)
             {
                 student = new Student();
-                Console.Write("ID = ");
+                Console.WriteLine("Student {0}:", i + 1);
+                Console.Write("ID: ");
                 student.StudentID = int.Parse(Console.ReadLine());
-                //tuong tu nhap cho cac thuoc tinh khac
+                Console.Write("Name: ");
+                student.FullName = Console.ReadLine();
+                Console.Write("Mark: ");
+                student.Mark = float.Parse(Console.ReadLine());
+                Console.Write("Faculty: ");
+                student.Faculty = Console.ReadLine();
+                Console.WriteLine("---End A Student---\n");
                 //dua doi tuong vao mang
                 studentList[i] = student;
+            }
+        }
+        private static void ShowStudentList(Student[] studentList)
+        {
+            foreach (Student st in studentList)
+            {
+                Console.Write("ID: {0}\nName: {1}\nMark: {2}\nFaculty: {3}", st.StudentID, st.FullName, st.Mark, st.Faculty);
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowFacultyCNTT()
+        {
+            Student[] listKQ = new Student[numOfStudent];
+            listKQ = studentList.Where(p => p.Faculty == "CNTT").ToArray();
+            if (listKQ.Count() > 0)
+            {
+                Console.WriteLine("\n=====SHOW STUDENT LIST IN THE FACULTY 'CNTT'=====");
+                ShowStudentList(listKQ);
+            }
+            else
+            {
+                Console.WriteLine("There are no students in the faculty CNTT!\n");
             }
         }
     }
