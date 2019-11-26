d01fc764055ae9de3a20bf2ec791b2e8a3cd2446
LINH DO <LINH DO@DESKTOP-JLC2ONE>
2019-11-26 08:27:23 +0700

    bai tap ngay 26

---
+++ UngDungMayTinh/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace UngDungMayTinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPhepCong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.btnPhepTru = new System.Windows.Forms.Button();
+            this.btnPhepNhan = new System.Windows.Forms.Button();
+            this.btnPhepChia = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.errSoA = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errSoA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPhepCong
+            // 
+            this.btnPhepCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepCong.Location = new System.Drawing.Point(229, 231);
+            this.btnPhepCong.Name = "btnPhepCong";
+            this.btnPhepCong.Size = new System.Drawing.Size(79, 67);
+            this.btnPhepCong.TabIndex = 0;
+            this.btnPhepCong.Text = "+";
+            this.btnPhepCong.UseVisualStyleBackColor = true;
+            this.btnPhepCong.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoA.Location = new System.Drawing.Point(229, 120);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(333, 30);
+            this.txtSoA.TabIndex = 3;
+            this.txtSoA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtSoA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoA_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Pink;
+            this.label2.Location = new System.Drawing.Point(127, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(435, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chương Trình Máy Tính";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Số B";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoB.Location = new System.Drawing.Point(229, 173);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(333, 30);
+            this.txtSoB.TabIndex = 3;
+            this.txtSoB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnPhepTru
+            // 
+            this.btnPhepTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepTru.Location = new System.Drawing.Point(314, 231);
+            this.btnPhepTru.Name = "btnPhepTru";
+            this.btnPhepTru.Size = new System.Drawing.Size(79, 67);
+            this.btnPhepTru.TabIndex = 0;
+            this.btnPhepTru.Text = "-";
+            this.btnPhepTru.UseVisualStyleBackColor = true;
+            this.btnPhepTru.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // btnPhepNhan
+            // 
+            this.btnPhepNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepNhan.Location = new System.Drawing.Point(399, 231);
+            this.btnPhepNhan.Name = "btnPhepNhan";
+            this.btnPhepNhan.Size = new System.Drawing.Size(79, 67);
+            this.btnPhepNhan.TabIndex = 0;
+            this.btnPhepNhan.Text = "*";
+            this.btnPhepNhan.UseVisualStyleBackColor = true;
+            this.btnPhepNhan.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // btnPhepChia
+            // 
+            this.btnPhepChia.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepChia.Location = new System.Drawing.Point(484, 231);
+            this.btnPhepChia.Name = "btnPhepChia";
+            this.btnPhepChia.Size = new System.Drawing.Size(79, 67);
+            this.btnPhepChia.TabIndex = 0;
+            this.btnPhepChia.Text = "/";
+            this.btnPhepChia.UseVisualStyleBackColor = true;
+            this.btnPhepChia.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(129, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 31);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kết Quả";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(349, 320);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(213, 30);
+            this.txtKetQua.TabIndex = 3;
+            this.txtKetQua.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // errSoA
+            // 
+            this.errSoA.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 389);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPhepChia);
+            this.Controls.Add(this.btnPhepNhan);
+            this.Controls.Add(this.btnPhepTru);
+            this.Controls.Add(this.btnPhepCong);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errSoA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPhepCong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.Button btnPhepTru;
+        private System.Windows.Forms.Button btnPhepNhan;
+        private System.Windows.Forms.Button btnPhepChia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.ErrorProvider errSoA;
+    }
+}
+
---
+++ UngDungMayTinh/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UngDungMayTinh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPhepTinh_Click(object sender, EventArgs e)
+        {
+            //Chuyển sender thành nút button
+            Button btnPhepTinh = (Button)sender;
+            // Xác định nút đang được bấm là nút nào dựa vào tên
+            string phepTinh = btnPhepTinh.Name;
+            //Sử dụng cấu trúc lựa chọn để xác định KQ
+            double KQ = 0;
+            double soA = double.Parse(txtSoA.Text);
+            double soB = double.Parse(txtSoB.Text);
+            switch (phepTinh)
+            {
+                case "btnPhepCong":
+                    KQ = soA + soB;
+                    break;
+
+                case
+                    "btnPhepTru":
+                    KQ = soA - soB;
+                    break;
+
+                case
+                    "btnPhepNhan":
+                    KQ = soA * soB;
+                    break;
+
+                case
+                    "btnPhepChia":
+                    KQ = soA / soB;
+                    break;
+
+                default:
+                    break;
+            }
+            txtKetQua.Text = KQ.ToString();
+        }
+
+        private void txtSoA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !(e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
