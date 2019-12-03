5de3675c93b081fd6e1be75732a085ac41f0d982
Tài Trần <transytai12061999@gmail.com>
2019-12-03 08:04:10 +0700

    ATK, Nộp bài

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,6 +12,10 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        public Form1()
+        {
+            InitializeComponent();
+        }
         private void btnCaluculator_Click(object sender, EventArgs e)
         {
             //Chuyển sender sang button
