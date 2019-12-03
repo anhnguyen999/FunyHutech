303e3d2f56302261961aff3a2e3fa373f9f68554
Varan <35282076+vuongvicent@users.noreply.github.com>
2019-12-03 07:59:35 +0700

    hoan thien code buoi 7

--- UngDungMayTinh/Form1.cs
+++ UngDungMayTinh/Form1.cs
@@ -12,21 +12,30 @@ namespace UngDungMayTinh
 {
     public partial class Form1 : Form
     {
+        string mgsError;
         public Form1()
         {
+            
             InitializeComponent();
         }
 
         private void BtnPhepTinh_Click(object sender, EventArgs e)
         {
+            double soA;
+            double soB;
+            
+            if (!ValidateInputNumber(out soA, out soB))
+            {
+                MessageBox.Show(mgsError, "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             // CHUYEN SENDER THANH BUTTON 
             Button btnPhepTinh = (Button)sender;
             // xac dinh nut dang duoc bam la nut nao dua vao ten 
             string phepTinh = btnPhepTinh.Name;
             //Sử dụng cấu trúc lựa chọn để xác định kết quả
             double ketQua = 0;
-            double soA = double.Parse (txtSoA.Text);
-            double soB = double.Parse(txtSoB.Text);
+
             switch (phepTinh)
             {
                 case "btnPhepCong" :
@@ -49,10 +58,29 @@ namespace UngDungMayTinh
 
         }
 
+        private bool ValidateInputNumber(out double soA, out double soB)
+        {
+            mgsError = "";
+            if (double.TryParse(txtSoA.Text, out soA))
+            {
+                mgsError = "So A không hợp lệ!";
+            }
+            if (double.TryParse(txtSoB.Text, out soB))
+            {
+                mgsError += "\nSo B không hợp lệ!";
+            }
+
+            if (mgsError == "")
+                return true;
+            return false;
+        }
+
         private void TxtNumber_KeyPress(object sender, KeyPressEventArgs e)
         {
             TextBox txtNumber = (TextBox)sender;
-            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == '.'))
+            //if (!(char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == '.'))
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+       (e.KeyChar != '.') || ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1)))
             {
                 e.Handled = true;
                 errorCheckNumber.SetError(txtNumber, "Không được nhập chữ hoặc khoảng trống");
