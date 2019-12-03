db4c520210c7e3decaa369645d5e785ca384202d
Tran Thi Anh Thu <bianh1805@gmail.com>
2019-12-03 09:10:18 +0700

    Buoi 6 lan 2

---
+++ buoi 6/Lab02_04/Lab02_04/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Lab02_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_MaSv = new System.Windows.Forms.Label();
+            this.lbl_HoTen = new System.Windows.Forms.Label();
+            this.lbl_ChuyenNganh = new System.Windows.Forms.Label();
+            this.lbl_GioiTinh = new System.Windows.Forms.Label();
+            this.lbl_ChonMon = new System.Windows.Forms.Label();
+            this.txt_MaSV = new System.Windows.Forms.TextBox();
+            this.txt_HoTen = new System.Windows.Forms.TextBox();
+            this.cb_ChuyenNganh = new System.Windows.Forms.ComboBox();
+            this.chb_Nam = new System.Windows.Forms.CheckBox();
+            this.chb_Nu = new System.Windows.Forms.CheckBox();
+            this.gb_TT = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.lb_2 = new System.Windows.Forms.ListBox();
+            this.lb_tenMon = new System.Windows.Forms.ListBox();
+            this.dgv_2 = new System.Windows.Forms.DataGridView();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ht = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gb_TT.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_MaSv
+            // 
+            this.lbl_MaSv.AutoSize = true;
+            this.lbl_MaSv.Location = new System.Drawing.Point(86, 22);
+            this.lbl_MaSv.Name = "lbl_MaSv";
+            this.lbl_MaSv.Size = new System.Drawing.Size(53, 20);
+            this.lbl_MaSv.TabIndex = 0;
+            this.lbl_MaSv.Text = "MaSV";
+            // 
+            // lbl_HoTen
+            // 
+            this.lbl_HoTen.AutoSize = true;
+            this.lbl_HoTen.Location = new System.Drawing.Point(86, 58);
+            this.lbl_HoTen.Name = "lbl_HoTen";
+            this.lbl_HoTen.Size = new System.Drawing.Size(61, 20);
+            this.lbl_HoTen.TabIndex = 1;
+            this.lbl_HoTen.Text = "Ho Ten";
+            // 
+            // lbl_ChuyenNganh
+            // 
+            this.lbl_ChuyenNganh.AutoSize = true;
+            this.lbl_ChuyenNganh.Location = new System.Drawing.Point(86, 98);
+            this.lbl_ChuyenNganh.Name = "lbl_ChuyenNganh";
+            this.lbl_ChuyenNganh.Size = new System.Drawing.Size(114, 20);
+            this.lbl_ChuyenNganh.TabIndex = 2;
+            this.lbl_ChuyenNganh.Text = "Chuyen Nganh";
+            // 
+            // lbl_GioiTinh
+            // 
+            this.lbl_GioiTinh.AutoSize = true;
+            this.lbl_GioiTinh.Location = new System.Drawing.Point(86, 138);
+            this.lbl_GioiTinh.Name = "lbl_GioiTinh";
+            this.lbl_GioiTinh.Size = new System.Drawing.Size(67, 20);
+            this.lbl_GioiTinh.TabIndex = 3;
+            this.lbl_GioiTinh.Text = "Gioi tinh";
+            // 
+            // lbl_ChonMon
+            // 
+            this.lbl_ChonMon.AutoSize = true;
+            this.lbl_ChonMon.Location = new System.Drawing.Point(86, 170);
+            this.lbl_ChonMon.Name = "lbl_ChonMon";
+            this.lbl_ChonMon.Size = new System.Drawing.Size(206, 20);
+            this.lbl_ChonMon.TabIndex = 4;
+            this.lbl_ChonMon.Text = "Chon cac mon hoc tham gia";
+            // 
+            // txt_MaSV
+            // 
+            this.txt_MaSV.Location = new System.Drawing.Point(204, 19);
+            this.txt_MaSV.Name = "txt_MaSV";
+            this.txt_MaSV.Size = new System.Drawing.Size(375, 26);
+            this.txt_MaSV.TabIndex = 5;
+            // 
+            // txt_HoTen
+            // 
+            this.txt_HoTen.Location = new System.Drawing.Point(204, 52);
+            this.txt_HoTen.Name = "txt_HoTen";
+            this.txt_HoTen.Size = new System.Drawing.Size(375, 26);
+            this.txt_HoTen.TabIndex = 6;
+            // 
+            // cb_ChuyenNganh
+            // 
+            this.cb_ChuyenNganh.FormattingEnabled = true;
+            this.cb_ChuyenNganh.Location = new System.Drawing.Point(204, 98);
+            this.cb_ChuyenNganh.Name = "cb_ChuyenNganh";
+            this.cb_ChuyenNganh.Size = new System.Drawing.Size(375, 28);
+            this.cb_ChuyenNganh.TabIndex = 7;
+            // 
+            // chb_Nam
+            // 
+            this.chb_Nam.AutoSize = true;
+            this.chb_Nam.Location = new System.Drawing.Point(204, 138);
+            this.chb_Nam.Name = "chb_Nam";
+            this.chb_Nam.Size = new System.Drawing.Size(68, 24);
+            this.chb_Nam.TabIndex = 8;
+            this.chb_Nam.Text = "Nam";
+            this.chb_Nam.UseVisualStyleBackColor = true;
+            // 
+            // chb_Nu
+            // 
+            this.chb_Nu.AutoSize = true;
+            this.chb_Nu.Location = new System.Drawing.Point(347, 138);
+            this.chb_Nu.Name = "chb_Nu";
+            this.chb_Nu.Size = new System.Drawing.Size(55, 24);
+            this.chb_Nu.TabIndex = 9;
+            this.chb_Nu.Text = "Nu";
+            this.chb_Nu.UseVisualStyleBackColor = true;
+            // 
+            // gb_TT
+            // 
+            this.gb_TT.Controls.Add(this.dataGridView1);
+            this.gb_TT.Controls.Add(this.btn_Xoa);
+            this.gb_TT.Controls.Add(this.btn_Luu);
+            this.gb_TT.Controls.Add(this.btn_2);
+            this.gb_TT.Controls.Add(this.btn_1);
+            this.gb_TT.Controls.Add(this.lb_2);
+            this.gb_TT.Controls.Add(this.lb_tenMon);
+            this.gb_TT.Controls.Add(this.txt_MaSV);
+            this.gb_TT.Controls.Add(this.lbl_ChonMon);
+            this.gb_TT.Controls.Add(this.chb_Nu);
+            this.gb_TT.Controls.Add(this.lbl_MaSv);
+            this.gb_TT.Controls.Add(this.chb_Nam);
+            this.gb_TT.Controls.Add(this.lbl_HoTen);
+            this.gb_TT.Controls.Add(this.cb_ChuyenNganh);
+            this.gb_TT.Controls.Add(this.lbl_GioiTinh);
+            this.gb_TT.Controls.Add(this.txt_HoTen);
+            this.gb_TT.Controls.Add(this.lbl_ChuyenNganh);
+            this.gb_TT.Location = new System.Drawing.Point(51, 12);
+            this.gb_TT.Name = "gb_TT";
+            this.gb_TT.Size = new System.Drawing.Size(625, 331);
+            this.gb_TT.TabIndex = 10;
+            this.gb_TT.TabStop = false;
+            this.gb_TT.Text = "Thong tin Sv";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-16, 331);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Location = new System.Drawing.Point(347, 285);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(168, 40);
+            this.btn_Xoa.TabIndex = 15;
+            this.btn_Xoa.Text = "Xoa Chon";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Location = new System.Drawing.Point(124, 285);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(206, 40);
+            this.btn_Luu.TabIndex = 14;
+            this.btn_Luu.Text = "Luu thong tin";
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(297, 249);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 35);
+            this.btn_2.TabIndex = 13;
+            this.btn_2.Text = "<";
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(297, 194);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 37);
+            this.btn_1.TabIndex = 12;
+            this.btn_1.Text = ">";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // lb_2
+            // 
+            this.lb_2.FormattingEnabled = true;
+            this.lb_2.ItemHeight = 20;
+            this.lb_2.Location = new System.Drawing.Point(410, 194);
+            this.lb_2.Name = "lb_2";
+            this.lb_2.Size = new System.Drawing.Size(169, 84);
+            this.lb_2.TabIndex = 11;
+            // 
+            // lb_tenMon
+            // 
+            this.lb_tenMon.FormattingEnabled = true;
+            this.lb_tenMon.ItemHeight = 20;
+            this.lb_tenMon.Items.AddRange(new object[] {
+            "Co so du lieu",
+            "Co so du lieu NC",
+            "PTTK he thong thong tin"});
+            this.lb_tenMon.Location = new System.Drawing.Point(103, 194);
+            this.lb_tenMon.Name = "lb_tenMon";
+            this.lb_tenMon.Size = new System.Drawing.Size(189, 84);
+            this.lb_tenMon.TabIndex = 10;
+            // 
+            // dgv_2
+            // 
+            this.dgv_2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.Ht,
+            this.CN,
+            this.Sex,
+            this.SM});
+            this.dgv_2.Location = new System.Drawing.Point(25, 367);
+            this.dgv_2.Name = "dgv_2";
+            this.dgv_2.RowHeadersWidth = 62;
+            this.dgv_2.RowTemplate.Height = 28;
+            this.dgv_2.Size = new System.Drawing.Size(749, 150);
+            this.dgv_2.TabIndex = 11;
+            // 
+            // STT
+            // 
+            this.STT.HeaderText = "MaSV";
+            this.STT.MinimumWidth = 8;
+            this.STT.Name = "STT";
+            this.STT.Width = 150;
+            // 
+            // Ht
+            // 
+            this.Ht.HeaderText = "Ho Ten";
+            this.Ht.MinimumWidth = 8;
+            this.Ht.Name = "Ht";
+            this.Ht.Width = 150;
+            // 
+            // CN
+            // 
+            this.CN.HeaderText = "Chuyen Nganh";
+            this.CN.MinimumWidth = 8;
+            this.CN.Name = "CN";
+            this.CN.Width = 150;
+            // 
+            // Sex
+            // 
+            this.Sex.HeaderText = "Gioi Tinh";
+            this.Sex.MinimumWidth = 8;
+            this.Sex.Name = "Sex";
+            this.Sex.Width = 150;
+            // 
+            // SM
+            // 
+            this.SM.HeaderText = "So Mon";
+            this.SM.MinimumWidth = 8;
+            this.SM.Name = "SM";
+            this.SM.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.dgv_2);
+            this.Controls.Add(this.gb_TT);
+            this.Name = "Form1";
+            this.Text = "Nhap Lieu Sinh Vien";
+            this.gb_TT.ResumeLayout(false);
+            this.gb_TT.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_MaSv;
+        private System.Windows.Forms.Label lbl_HoTen;
+        private System.Windows.Forms.Label lbl_ChuyenNganh;
+        private System.Windows.Forms.Label lbl_GioiTinh;
+        private System.Windows.Forms.Label lbl_ChonMon;
+        private System.Windows.Forms.TextBox txt_MaSV;
+        private System.Windows.Forms.TextBox txt_HoTen;
+        private System.Windows.Forms.ComboBox cb_ChuyenNganh;
+        private System.Windows.Forms.CheckBox chb_Nam;
+        private System.Windows.Forms.CheckBox chb_Nu;
+        private System.Windows.Forms.GroupBox gb_TT;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Luu;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.ListBox lb_2;
+        private System.Windows.Forms.ListBox lb_tenMon;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv_2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ht;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SM;
+    }
+}
+
