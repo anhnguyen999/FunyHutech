7a250c9144f6858421061ea9c683deb202df0bb7
Tài Trần <transytai12061999@gmail.com>
2019-12-17 09:00:14 +0700

    Nộp bài 17/12/2019 ATK

---
+++ GUI_QuanLySinhVien/frmQuanLySinhVien.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace GUI_QuanLySinhVien
+{
+    partial class frmQuanLySinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.cbChuyenNganh = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(95, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã SV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(91, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(24, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chuyên Ngành";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMaSV.Location = new System.Drawing.Point(203, 31);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(430, 30);
+            this.txtMaSV.TabIndex = 3;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtHoTen.Location = new System.Drawing.Point(203, 87);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(430, 30);
+            this.txtHoTen.TabIndex = 4;
+            // 
+            // cbChuyenNganh
+            // 
+            this.cbChuyenNganh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cbChuyenNganh.FormattingEnabled = true;
+            this.cbChuyenNganh.Location = new System.Drawing.Point(203, 145);
+            this.cbChuyenNganh.Name = "cbChuyenNganh";
+            this.cbChuyenNganh.Size = new System.Drawing.Size(430, 33);
+            this.cbChuyenNganh.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(302, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmQuanLySinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 295);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbChuyenNganh);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmQuanLySinhVien";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.ComboBox cbChuyenNganh;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GUI_QuanLySinhVien/frmQuanLySinhVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS_QuanLySinhVien;
+
+namespace GUI_QuanLySinhVien
+{
+    public partial class frmQuanLySinhVien : Form
+    {
+        BUS_ChuyenNganh bus_ChuyenNganh = new BUS_ChuyenNganh();
+        public frmQuanLySinhVien()
+        {
+            InitializeComponent();
+            ControlConfig();
+        }
+        private void ControlConfig()
+        {
+            cbChuyenNganh.DisplayMember = "TenChuyenNganh";
+            cbChuyenNganh.ValueMember = "MaChuyenNganh";
+        }
+
+        private void FrmQuanLySinhVien_Load(object sender, EventArgs e)
+        {
+            LoadChuyenNganh();
+        }
+        private void LoadChuyenNganh()
+        {
+            cbChuyenNganh.DataSource = bus_ChuyenNganh.GetChuyenNganh();
+        }
+    }
+}
