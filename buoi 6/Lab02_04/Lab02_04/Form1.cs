8f84030af6cfb83c5476d7a3ae4857c863d6c03d
Tran Thi Anh Thu <bianh1805@gmail.com>
2019-12-17 09:13:06 +0700

    Buoi 8 BL

--- buoi 6/Lab02_04/Lab02_04/Form1.cs
+++ buoi 6/Lab02_04/Lab02_04/Form1.cs
@@ -7,16 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS_QuanLySinhVien;
 
-namespace Lab02_04
+namespace GUI_QuanLySinhVien
 {
     public partial class Form1 : Form
-    {
+    { BUS_ChuyenNganh bus_ChuyenNganh = new BUS_ChuyenNganh();
         public Form1()
         {
             InitializeComponent();
+            ControlConfig();
         }
 
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
+        }
+        private void ControlConfig()
+        {
+            cbChuyenNganh.DisplayMember = "TenCN";
+            cbChuyenNganh.ValueMember = "MACN";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadChuyenNganh();
+        }
+        private void LoadChuyenNganh()
+         {
+                cbChuyenNganh.DataSource = bus_ChuyenNganh.GetChuyenNganh();
+         }
     }
 }
