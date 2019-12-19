9a060493978c537cca14f99020371122e41b1e52
Tran Thi Anh Thu <bianh1805@gmail.com>
2019-12-19 16:11:35 +0700

    nopbai

---
+++ BUS_QuanLySinhVien/BUS_ChuyenNganh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_QuanLySinhVien;
+
+namespace BUS_QuanLySinhVien
+{
+    public class BUS_ChuyenNganh
+    {
+        QuanLySinhVienEntities dbContext = new QuanLySinhVienEntities();
+        public BUS_ChuyenNganh()
+        {
+
+        }//
+        public List<ChuyenNganh> GetChuyenNganh()
+        {
+            return dbContext.ChuyenNganhs.ToList();
+        }
+    }
+
+}
